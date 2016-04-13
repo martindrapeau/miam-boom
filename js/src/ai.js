ee58e9e23d171d3a5b4e5a9c9a47c0bf4804313a
Martin Drapeau <martindrapeau@gmail.com>
2016-04-12 22:28:20 -0400

    WIP Various throw heuristics

--- js/src/ai.js
+++ js/src/ai.js
@@ -5,24 +5,22 @@
       name: "ai",
       state: "idle"
     },
+    delayThrowRandom: function() {
+      var fruits = this.world.get("fruits"),
+          startDelay = Math.max(150, 350 - fruits * 2),
+          deltaDelay = Math.max(500, 1500 - fruits * 20),
+          delay = Math.floor(startDelay + deltaDelay*Math.random());
+      this._throwTime = _.now();
+      this._throwDelay = delay;
+    },
     heuristics: {
-      _delayRandom: function() {
-        var fruits = this.world.get("fruits"),
-            startDelay = Math.max(150, 350 - fruits * 2),
-            deltaDelay = Math.max(500, 1500 - fruits * 20),
-            delay = Math.floor(startDelay + deltaDelay*Math.random());
-        this._throwTime = _.now();
-        this._throwDelay = delay;
-      },
       idle: function(options) {
-        this.stop();
+        this._throwDelay = undefined;
       },
       random: function(options) {
         options || (options = {});
-        var hero = this.world.getHero();
 
-        if (hero && !hero.isDisabled() && !options.skip) {
-
+        if (!options.skip) {
           var index = Math.floor((Backbone.fruitNames.length-0.01)*Math.random()),
               fruitName = Backbone.fruitNames[index],
               fruitClass = Backbone[_.classify(fruitName)],
@@ -42,14 +40,12 @@
           this.world.add(fruit);
         }
 
-        this.heuristics._delayRandom.call(this);
+        this.delayThrowRandom();
       },
       sirup: function(options) {
         options || (options = {});
-        var hero = this.world.getHero();
 
-        if (hero && !hero.isDisabled() && !options.skip) {
-
+        if (!options.skip) {
           this._sirupX || (this._sirupX = Backbone.WIDTH*0.10);
           this._sirupDir || (this._sirupDir = "right");
 
@@ -83,10 +79,8 @@
       },
       rain: function(options) {
         options || (options = {});
-        var hero = this.world.getHero();
 
-        if (hero && !hero.isDisabled() && !options.skip) {
-
+        if (!options.skip) {
           var index = Math.floor((Backbone.fruitNames.length-0.01)*Math.random()),
               fruitName = Backbone.fruitNames[index],
               fruitClass = Backbone[_.classify(fruitName)],
@@ -103,14 +97,12 @@
           this.world.add(fruit);
         }
 
-        this.heuristics._delayRandom.call(this);
+        this.delayThrowRandom();
       },
       rain2: function(options) {
         options || (options = {});
-        var hero = this.world.getHero();
 
-        if (hero && !hero.isDisabled() && !options.skip) {
-
+        if (!options.skip) {
           var index = Math.floor((Backbone.fruitNames.length-0.01)*Math.random()),
               fruitName = Backbone.fruitNames[index],
               fruitClass = Backbone[_.classify(fruitName)],
@@ -128,14 +120,28 @@
           this.world.add(fruit);
         }
 
-        this.heuristics._delayRandom.call(this);
+        this.delayThrowRandom();
       }
     },
     initialize: function(attributes, options) {
       this._throwTime = undefined;
       this._throwDelay = undefined;
+      this._stateChangeTime = undefined;
+      this._stateChangeDelay = undefined;
       this.world = options.world;
       this.listenTo(this.world, "change:state", this.onWorldChangeState);
+
+      this.heuristicNames = ["random", "rain", "sirup", "rain2"];
+      this.heuristicNames.index = 0;
+      this.heuristicNames.next = function() {
+        var index = this.index;
+        this.index += 1;
+        if (this.index >= this.length) this.index = 0;
+        return this[index];
+      }.bind(this.heuristicNames);
+      this.heuristicNames.reset = function() {
+        this.index = 0;
+      }.bind(this.heuristicNames);
     },
     onWorldChangeState: function() {
       var state = this.world.get("state");
@@ -143,20 +149,47 @@
       this._sirupX = this._sirupDir = undefined;
     },
     start: function(options) {
-      this.set({state: "random"});
+      this.set({state: "idle"});
       this.throwFruit(options);
+      this.changeState(options);
     },
     stop: function() {
       this.set({state: "idle"});
       this._throwDelay = undefined;
+      this._stateChangeDelay = undefined;
+      this.heuristicNames.reset();
     },
     throwFruit: function(options) {
       var state = this.get("state"),
           fn = this.heuristics[state];
       if (typeof fn == "function") return fn.apply(this, arguments);
     },
+    changeState: function(options) {
+      options || (options = {});
+
+      if (!options.skip) {
+        var state = this.get("state"),
+            index = state == "idle" ? Math.floor((this.heuristicNames.length-0.01)*Math.random()) : null,
+            newState = index != null ? this.heuristicNames.next() : "idle";
+        console.log("HEURISTIC", newState);
+        this.set({state: newState});
+        this.throwFruit();
+      }
+
+      this._stateChangeTime = _.now();
+      this._stateChangeDelay = this.get("state") == "idle" ? 2000 : 15000;
+    },
     update: function(dt) {
-      if (this._throwTime && this._throwDelay && _.now() > this._throwTime + this._throwDelay) this.throwFruit();
+      var now = _.now();
+
+      if (this._throwTime && this._throwDelay &&
+          now > this._throwTime + this._throwDelay)
+        this.throwFruit();
+
+      if (this._stateChangeTime && this._stateChangeDelay &&
+          now > this._stateChangeTime + this._stateChangeDelay)
+        this.changeState();
+
       return false;
     },
     draw: function() {}
